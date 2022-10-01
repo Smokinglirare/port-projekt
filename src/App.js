3d2f99d82bc16cb4be4af2ac7f4bd28fe850df4f
Sebastian Torneus <Sebastian.thorneus@gmail.com>
2022-10-01 03:06:52 +0200

    footer fixad

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import PortfolioPage from './Pages/PortfolioPage/PortfolioPage';
 import Home from './Pages/Home/Home';
 import Navbar from './Components/Navbar/Navbar/Navbar';
+import FooterNav from './Components/FooterNav/FooterNav';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
     <Route path="/portfolio" exact element={<PortfolioPage />} />
 
       </Routes>
-      
+      <FooterNav />
       </Router> 
       
     </div>
---
+++ src/Components/FooterNav/FooterNav.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { navItems, socialIcons, Logo } from '../Navbar/NavbarComponents';
+import "./FooterNav.css"
+
+const FooterNav = () => {
+    return (
+        <div className="footer-container">
+            <div className="footer-nav-container">
+                <div className="footer-navigation">
+                    {navItems.map(({ name, id }) => (<a className="footer-nav-item" href={id}>
+                        {name}
+                    </a>
+                    ))}
+                </div>
+                <div className="footer-socials">
+                    {socialIcons.map((icon) => (
+                        <a className="footer-social-icon" href="#">
+                            {icon}
+                        </a>
+                    ))}
+                </div>
+            </div>
+            <div className="footer-logo-container">{Logo} </div>
+            <h6>Copyright @2022 Thornes All rights reserved.</h6>
+        </div>
+    );
+};
+
+export default FooterNav;
