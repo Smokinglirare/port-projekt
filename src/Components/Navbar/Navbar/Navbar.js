06a4f7d4804b286ee3804d6ec299308f628e6559
Sebastian Torneus <Sebastian.thorneus@gmail.com>
2022-10-02 16:33:49 +0200

    fixat länkar

--- src/Components/Navbar/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
     window.addEventListener("scroll", closeMenu);
 
     const hamburger = (
-        <a href="http://www.google.com" className="hamburger-container" onClick={() => setNavMenu(!navMenu)}>
+        <a  className="hamburger-container" onClick={() => setNavMenu(!navMenu)}>
             <div className={navMenu ? "close-hamburger" : "hamburger"}>
                 <span className="ham-span"></span>
                 <span className="ham-span"></span>
@@ -37,7 +37,7 @@ const Navbar = () => {
         <div className="navbar">
             <div className="nav-icon-container">
                 {socialIcons.map((icon) => (
-                    <a className="nav-icon" href="http://www.google.com">
+                    <a className="nav-icon" >
                         {icon}
                     </a>
                 ))}
